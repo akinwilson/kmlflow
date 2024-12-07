04a2f777009e435652ca6df13165914b62a8991e
ola <battlestation.home>
2024-12-07 13:03:02 +0000

    allowing for post requests to be saved

--- 1building_images/server.py
+++ 1building_images/server.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi import Request
+
 import logging 
 
 logger = logging.getLogger('uvicorn.error')
@@ -11,4 +13,24 @@ app = FastAPI()
 async def root():
     logger.info("Hit root URL.")
     return {"message": "testing out building dockerised images"}
+
+
+
+@app.post('/volumes/')
+async def save_data(request: Request):
+    payload = await request.json()
+    
+    logger.info(f"Hitting saving data to presistent volumes.\n{payload}")
+    data =payload['message']
+    with open('./data/testing.txt', "a+") as file:
+        file.write(data + "\n")
+                
+    return {"message": "Data has been written to presistent volume"}
+
+    
+
+@app.get('/healthy')
+async def liveness():
+    logger.info("Livenesss health check made")
+    return {"message": "The server is alive and functioning as expected"}
 
