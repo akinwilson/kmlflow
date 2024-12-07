db8b41a6a360cb76f2925c3f5d68b070e36faff1
ola <battlestation.home>
2024-12-07 10:30:02 +0000

    adding logging picking up uvicorn.error logger to enable application-level logging from outside of the container

--- 1building_images/server.py
+++ 1building_images/server.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+import logging 
 
+logger = logging.getLogger('uvicorn.error')
+                   
+logger.info("Tester application has started")
 
 app = FastAPI()
 
 @app.get('/')
 async def root():
+    logger.info("Hit root URL.")
     return {"message": "testing out building dockerised images"}
+
