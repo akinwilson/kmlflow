f893ad38a8a32aa94bc7bb2714e0ebc97cb0011f
ola <battlestation.home>
2024-12-24 07:54:11 +0000

    added colourised terminal outputs for hyperlinks to UIs

--- app/katlib/deploy.sh
+++ app/katlib/deploy.sh
@@ -1,29 +1,42 @@
+#!/usr/bin/env bash
+
 set -e 
 
-# 
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+GREEN='\033[0;32m'
+RESET='\033[0m'
+
+
 echo "Deploying local mulit-node kubernetes cluster using kind ..."
-kind create cluster --config kind_cluster_deployment.yaml || 
+kind create cluster --config  "$SCRIPT_DIR/kind_cluster_deployment.yaml" || 
 
 
 # 
 echo "Setting context for kubectl cli tool ..."
 kubectl cluster-info --context kind-kf-cluster
+echo ""
 
 echo "Rolling out presistent volume and presistent volume claim for Katlib to use as backend storage ..."
-kubectl apply -f presistent_volume_and_claim.yaml 
+kubectl apply -f "$SCRIPT_DIR/presistent_volume_and_claim.yaml" 
+echo ""
 
 echo "Rolling out cluster dashboard application ... "
-kubectl apply -f dashboard.yaml
+kubectl apply -f "$SCRIPT_DIR/dashboard.yaml"
+echo ""
 
 echo "Creating Service Account ...."
-kubectl apply -f admin.yaml
+kubectl apply -f "$SCRIPT_DIR/admin.yaml"
+echo ""
 
 echo "Allow all service accounts to view all resources .. "
-kubectl apply -f permissions.yaml
+kubectl apply -f "$SCRIPT_DIR/permissions.yaml"
+echo ""
 
 
 echo "Install Katlib ..."
 kubectl apply -k "github.com/kubeflow/katib.git/manifests/v1beta1/installs/katib-standalone?ref=master"
+echo ""
 
 echo "Set up proxy server to make cluster dashboard available ..."
 kubectl proxy & 
@@ -35,12 +48,20 @@ kubectl create token user
 
 
 echo ""
-echo "Head to:\n\nhttp://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/\n\nto view the k8s cluster health\n\n"
+echo "Head to:"
+echo ""
+echo -e "${GREEN}http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/${RESET}"
+echo ""
+echo "to view the k8s cluster health"
 
 
-echo "To access the katlib's user interface"
+echo ""
+echo ""
 
 kubectl port-forward svc/katib-ui -n kubeflow 8080:80 &
  
-echo "Accessing the UI through:\n\n"
-echo "http://localhost:8080/katib/"
+echo "To access the katib's user interface head to:"
+echo ""
+echo -e "${GREEN}http://localhost:8080/katib/${RESET}"
+echo ""
+
