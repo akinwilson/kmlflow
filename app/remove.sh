6d4059a9ebde4b5ee7ae7b4b8f3877276bd8a2c8
ola <battlestation.home>
2024-12-26 10:44:05 +0000

    added comment to script for removing cluster on env use

--- app/remove.sh
+++ app/remove.sh
@@ -1,5 +1,18 @@
 set -e 
 
 echo "Deleting cluster ..."
-
 kind delete cluster
+
+echo "Delete cluster context from kubectl ..."
+# Need to replace cluster name with env variable here
+kubectl config delete-cluster kind-kmflow-local-v1
+
+echo "Using docker client to remove cluster processes ... "
+docker stop $(docker ps -a -q)
+
+
+echo "Removing images ... "
+docker rmi -f $(docker images -aq)
+
+echo "Removing kind network"
+docker network rm kind 
